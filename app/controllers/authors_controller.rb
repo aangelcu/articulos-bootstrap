25c34769a61a774cdf2159d10ea0693c8cbe434f
andres <andresangelcuervo@gmail.com>
2014-12-05 14:55:07 -0500

    Cree los metodos para los posts y los authors

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,2 +1,18 @@
 class AuthorsController < ApplicationController
+	def create
+		@author = Author.new(author_params)
+
+		if @author.save
+			redirect_to @author, notice: 'Author was succesfuly created'
+		else
+			render :new
+		end
+	end
+
+	private
+	def author_params
+		params.require(:author).permit(:name)
+	end
+
 end
+
